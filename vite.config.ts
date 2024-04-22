6abb6370ad6723f57bd95dd171f33286eb1bf743
Anılcan Metinyurt <igoodie@programmer.net>
2024-04-22 23:59:35 +0300

    fix: SCSS assets not loading up

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import content from "@originjs/vite-plugin-content";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,4 +19,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "./src"),
+    },
+  },
 });

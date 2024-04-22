6c05739ed684db1aac64c3fec60a6efaaa35e4e1
Anılcan Metinyurt <igoodie@programmer.net>
2024-04-22 22:31:14 +0300

    chore: remove Fishing's WIP heading

--- src/app.main.tsx
+++ src/app.main.tsx
@@ -64,10 +64,6 @@ function App() {
           </SummarySection>
 
           <SummarySection>
-            <h1>Fishing [WIP]</h1>
-          </SummarySection>
-
-          <SummarySection>
             <h1>Socials [WIP]</h1>
           </SummarySection>
 
@@ -99,87 +95,6 @@ function App() {
           </SummarySection>
         </>
       )}
-
-      {/* <section id="fishing">
-        <h1 style={{ fontSize: 24 }}>Fish Collection</h1>
-
-        <div
-          style={{
-            display: "grid",
-            gap: 50,
-            gridTemplateColumns: `repeat(${farmerNames.length}, 1fr)`,
-          }}
-        >
-          {farmerNames.map((farmerName) => {
-            const caughtFishes = gameSave.getCaughtFishes(farmerName)!;
-
-            return (
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  gap: 4,
-                }}
-              >
-                <h2 style={{ fontSize: 12 }}>{farmerName}</h2>
-
-                {entries(caughtFishes).map(([categoryName, fishes]) => {
-                  return (
-                    <div
-                      style={{
-                        background: "#604620",
-                        borderRadius: 5,
-                        padding: "0 10px",
-                        display: "grid",
-                        gap: 10,
-                        gridTemplateColumns: "50px 1fr",
-                      }}
-                    >
-                      <h1
-                        style={{
-                          fontSize: 11,
-                          color:
-                            STARDEW_FISH_CATEGORIES[
-                              categoryName as FishCategory
-                            ].accentColor,
-                        }}
-                      >
-                        {categoryName}
-                      </h1>
-
-                      <div style={{ display: "flex", flexWrap: "wrap" }}>
-                        {fishes.map((fish) => (
-                          <div>
-                            <img
-                              width={34}
-                              alt="Fish"
-                              title={fish.name}
-                              src={
-                                // @ts-ignore
-                                fishSprites[
-                                  `./assets/sprite/fish/${lowerCase(
-                                    fish.name
-                                  ).replace(/\s+/g, "_")}.png`
-                                  // @ts-ignore
-                                ]?.default ?? ""
-                              }
-                              style={{
-                                filter:
-                                  fish.caught !== 0 ? "" : "brightness(0)",
-                                opacity: fish.caught !== 0 ? 1 : 0.2,
-                              }}
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            );
-          })}
-        </div>
-      </section> */}
 
       <footer style={{ height: 200 }}></footer>
     </main>
